87f61c85ba4873af7fd23924a3ceb7c19d4b5503
sonia526 <zamirastars3@gmail.com>
2017-07-17 12:09:51 -0500

    calcular masacon funciones

--- calculadora.js
+++ calculadora.js
@@ -1,4 +1,7 @@
- var peso= prompt("ingrese su peso") ;
- var altura= prompt("ingrese su altura");
+function calculadoraIMC(peso,altura){
+  peso= prompt("ingrese su peso") ;
+  altura= prompt("ingrese su altura");
  var imc =Math.round(peso/(altura*altura));
  alert("el resultado es : " + imc);
+}
+calculadoraIMC();
